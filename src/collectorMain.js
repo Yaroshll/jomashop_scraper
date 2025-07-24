152378ecb0be1a83df604b1376b18e81d2caacea
unknown <yaradayoub2019@gmail.com>
2025-07-24 05:51:47 +0300

    add urls and tags

--- src/collectorMain.js
+++ src/collectorMain.js
@@ -1,11 +1,33 @@
 import { collectProductUrls } from "./urlcollector.js";
 
 const inputObject = {
-  extraTags1: ["skincare", "body"],
-  url1: "https://www.jomashop.com/filters/skin-care-products?department=Skincare&beauty_group=Body",
-  extraTags2: ["makeup", "eyes"],
-  url2: "https://www.jomashop.com/eye-products.html",
-  // Add more URL and tag pairs as needed
+  extraTags1: ["Beauty", "women","Nails","Nail Polish","Nail Care"],
+  url1: "https://www.jomashop.com/filters/makeup?beauty_group=Hair%7CNails",
+  extraTags2: ["Beauty", "women","Lips","Lipstick","Lip Stain","Lip Plumpers","Lip Oil","Lip Liner","Gloss","Lip Treatments"],
+  url2: "https://www.jomashop.com/filters/lip-products?department=Makeup",
+  extraTags3: ["Makeup", "women","Bronzer","Concealer","Face Primer","Foundation","Highlighter","Setting Makeup","Blush","Face"],
+  url3: "jomashop.com/filters/face-products?department=Makeup",
+  extraTags4: ["Makeup", "women","Mascara", "Unisex","Eye Cream","Makeup Remover","Lash Serums","Eyeshadow","Eyeliner","Eyebrows","Eye Primer","Eyes"],
+  url4: "https://www.jomashop.com/filters/eye-products?department=Makeup%7CSkincare&gender=Unisex%7CWomens",
+  extraTags5: ["Makeup", "women","Lips","Sponges ","Brushes","Lash Tools","Blotting Papers","Makeup Tools"],
+  url5: "https://www.jomashop.com/filters/makeup?beauty_product_type=Styling+Tools%7CTools",
+  extraTags6: ["Makeup", "women","Scrubs & Exfoliants","Shower","Masks"],
+  url6: "https://www.jomashop.com/collections/makeup/Scrubs-Foams-And-Exfoliants-Makeup~YmVhdXR5X3Byb2R1Y3RfdHlwZX5TY3J1YnMlMkMlMjBGb2FtcyUyMCUyNiUyMEV4Zm9saWFudHM",
+  extraTags7: ["Makeup", "Sunscreen","Bronzing","Suncare","Eye Cream","Face Serums","Body Treatments","Face Moisturizer","Face Exfoliators","Suncare","Moisturizers","Night Cream","Body Creams","Body Care","Body Serums","Acne Treatments","Body Butters","Face Oils"],
+  url7: "https://www.jomashop.com/filters/skin-care-products?department=Skincare&gender=Womens",
+  extraTags8: ["Makeup","women", "Unisex","Lotion","Suncare","Eye Cream","Face Serums","Body Treatments","Face Moisturizer","Face","Bath & Shower","Moisturizers","Night Cream","Body Creams","Body Care","Body Serums","Acne Treatments","Body Butters","Setting Makeup","Hand & Foot"],
+  url8: "https://www.jomashop.com/filters/bath-body?gender=Unisex%7CWomens&subtype=Bath+%26+Body%7CHealth+%26+Wellness%7CMakeup&beauty_group=Body%7CHand+%26+Foot",
+  extraTags9:  ["Makeup","women","Masks", "Unisex","Kid's Haircare","Wax & Pomade","Volume &Texture","Smoothing","Hairspray","Gloss & Shine","Curl Enhancing","Hair Care","Hair Treatment","Styling Products","Heat Protectant","Hair Cleaner"],
+  url9: "https://www.jomashop.com/filters/mask?subtype=Hair+Care%7CHair&gender=Unisex%7CWomens",
+  extraTags10: ["Makeup", "women","Unisex",,"Sponges ","Brushes","Lash Tools","Blotting Papers","Makeup Tools"],
+  url10: "https://www.jomashop.com/tools-and-brushes.html?gender=Unisex%7CWomens&subtype=Makeup%7CTools+%26+Brushes",
+  extraTags11: ["Makeup","women", "Unisex","Kid's Haircare","Wax & Pomade","Volume &Texture","Smoothing","Hairspray","Gloss & Shine","Curl Enhancing","Hair Care","Hair Treatment","Styling Products","Heat Protectant","Hair Cleaner"],
+  url11: "https://www.jomashop.com/filters/hair-care?department=Hair+Care&gender=Unisex%7CWomens",
+  extraTags12: ["Makeup","women", "Toner","Makeup Remover","Face Wash","Balms & Oils","Lotion","Night Cream"],
+  url12: "https://www.jomashop.com/filters/cleanser?gender=Womens",
+  extraTags13: ["Makeup","women", "Unisex","Masks"],
+  url13: "https://www.jomashop.com/filters/mask?gender=Unisex%7CWomens&subtype=Skin+Care",
+ 
 };
 
 const MIN_DISCOUNT = 0;
