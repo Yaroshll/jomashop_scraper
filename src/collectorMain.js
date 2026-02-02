a613391adb5d7dfaac35c89512da9c73a7ceb7dc
unknown <yaradayoub2019@gmail.com>
2026-02-03 00:16:15 +0300

    test code for outofstock

--- src/collectorMain.js
+++ src/collectorMain.js
@@ -1,7 +1,7 @@
 import { collectProductUrls } from "./urlcollector.js";
 
 // Configuration - change these values as needed
-const TARGET_URL = "https://www.jomashop.com/filters/sunglasses?price=%7B%22from%22%3A100%2C%22to%22%3A300%7D&manufacturer=Alexander+Mcqueen%7CBalenciaga%7CBottega+Veneta%7CBurberry%7CBvlgari%7CCeline%7CChlo%C3%A9%7CChopard%7CDior%7CDolce+%26+Gabbana%7CEmporio+Armani%7CFendi%7CFerragamo%7CGivenchy%7CGucci%7CJimmy+Choo%7CLoewe%7CMaui+Jim%7CMoncler%7CMontblanc%7COff-White%7CPhilipp+Plein%7CPrada%7CPrada+Linea+Rossa%7CRay-Ban%7CSaint+Laurent%7CTom+Ford%7CVersace&sort=price_desc%7Cdesc"; // Example: Philipp Plein category
+const TARGET_URL = "https://www.jomashop.com/watches.html?sort=saving%7Cdesc&p=1"; // Example: Philipp Plein category
 const MIN_DISCOUNT = 40; // Minimum discount percentage (set to 0 to ignore discounts)
 
 async function main() {
